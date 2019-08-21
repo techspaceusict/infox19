ba75f8d8cd0eeee08ae8e82b21733492a2412bf7
vaideshshank <vaideshshanky@gmail.com>
2019-08-21 20:24:46 +0530

    updates

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express();
 const bodyParser=require('body-parser');
 const userRoutes=require('./routes/user');
 require('dotenv').config();
-const PORT=process.env.PORT;
+const PORT=process.env.PORT || 5000;
 
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.urlencoded({extended:false}));
