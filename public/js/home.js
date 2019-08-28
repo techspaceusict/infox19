a259f857baf1d0aeedf630e38c954b2eff8e06ce
vaideshshank <vaideshshanky@gmail.com>
2019-08-29 01:50:10 +0530

    changes

--- public/js/home.js
+++ public/js/home.js
@@ -1,22 +1,5 @@
 "use strict"
 
-var countDownDate = new Date ("october 15, 2019 00:00:00").getTime();
-    var x = setInterval(function() {
-    setTimeout(()=>{
-        var now = new Date ().getTime();
-        var distance = countDownDate - now;
-        var days = Math.floor (distance /(1000*60*60*24));
-        var hours = Math.floor ((distance % (1000*60*60*24)) / (1000*60*60));
-        var minutes = Math.floor ((distance % (1000*60*60)) / (1000*60));
-        var seconds = Math.floor ((distance % (1000*60)) / 1000);
-
-        document.getElementById("launch").innerHTML=days + "d " + hours + "h " + minutes + "m " + seconds + "s";
-        if (distance < 0) {
-        clearInterval(x);
-        document.getElementById('launch').innerHTML="EXPIRED";
-        }
-    },4000)
-}, 1000);
 
 
 // setTimeout(() => {
