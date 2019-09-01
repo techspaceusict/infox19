a3572a2740a0b7f7eb1ce7eaa58d4383b00aeef9
vaideshshank <vaideshshanky@gmail.com>
2019-09-02 01:41:18 +0530

    updates

--- public/js/home.js
+++ public/js/home.js
@@ -1,17 +1,50 @@
 "use strict"
 
 
+$(function(){
+      
+  $(".down-arrow").click(()=>{
+    location.hash="about";
+    $("html,body").animate({
+      scrollTop: $("#aboutus").offset().top
+    },1000)
+  });
 
-// setTimeout(() => {
-//     document.getElementById("smoke").remove();
-//     document.getElementsByTagName("body")[0].style.overflow="visible";
-// }, 7200);
+  $.get("public/templates/back-anime.html").then(data=>{
+    $("#hero").prepend(data);
+  });
+  location.hash="about";
+  $(window).scroll(()=>{
+    //console.log($("#hero").height()+$("#hero").offset().top);
+    if($(window).scrollTop()>$("#hero").height()+$("#hero").offset().top){
+      $(".navigation").addClass("revealNav");
+    }else{
+      $(".navigation").removeClass("revealNav");
+    }
+  });
 
+  setHash();
+  
+  
+});
+
+function setHash(){
+  let hash=location.hash;
+  console.log(hash);
+  $.get(`public/templates/${hash.substr(1)}.html`).then(data=>{
+    $("#routes").html(data);
+  })
+}
+
+window.onhashchange=setHash
+
+
+function changeRoute(obj){
+  location.hash=$(obj).text().split(" ").join("-").toLowerCase();
+}
 
 
 // AOS initialize
-
-
 AOS.init({
   // Global settings:
   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
@@ -30,10 +63,8 @@ AOS.init({
   duration: 400, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
   once: true, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
+  mirror: true, // whether elements should animate out while scrolling past them
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
 
-
-$(document).scrollTop(0);
