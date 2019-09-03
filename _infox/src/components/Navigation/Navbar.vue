2579dd42008eb8593f6ed139b013763226bd2798
Dhruv <dhruvmisra@live.com>
2019-09-04 00:44:34 +0530

    Added Navigation components

---
+++ _infox/src/components/Navigation/Navbar.vue
@@ -0,0 +1,119 @@
+<template>
+  <nav class="navbar navbar-dark justify-content-start" :class="{ 'navbar-admin': isAdmin }">
+    <TheSideNavToggle @toggle="$emit('sidenavToggle')" />
+
+    <div class="brand">
+      <nuxt-link to="/" class="navbar-brand my-2">{{ eventName }}</nuxt-link>
+    </div>
+
+    <div class="navigation-items">
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <nuxt-link to="/network" class="nav-link">Network</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/questions" class="nav-link">Questions</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/polls" class="nav-link">Polls</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/feedback" class="nav-link">Feedback</nuxt-link>
+        </li>
+      </ul>
+    </div>
+
+    <div class="ml-auto profile-dropdown">
+      <b-dropdown size="lg" variant="link" toggle-class="text-decoration-none" no-caret>
+        <div slot="button-content" class="fas fa-user profile card rounded-circle"></div>
+        <b-dropdown-item @click="$router.push('/profile')">Profile</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item @click="$store.dispatch('event/switchEvent')">Switch Event</b-dropdown-item>
+        <b-dropdown-item v-if="signedIn" @click="$store.dispatch('logout')">Logout</b-dropdown-item>
+      </b-dropdown>
+      <!-- <nuxt-link to="/profile" tag="div" class="profile card rounded-circle"></nuxt-link> -->
+    </div>
+  </nav>
+</template>
+
+<script>
+import TheSideNavToggle from "./TheSideNavToggle";
+
+export default {
+  name: "TheNavbar",
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  },
+  components: {
+    TheSideNavToggle
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  background: black;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+  padding: 0 3px;
+  text-align: left;
+  transition: background 1200ms ease-out;
+}
+.navbar-admin {
+  background: linear-gradient(180deg, #311905 0%, #722714 100%);
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+}
+.brand {
+  max-width: 190px;
+  font-size: 0.5em;
+  padding-right: 5px;
+  padding-left: 10px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.nav-item {
+  width: fit-content;
+  /* border: 1px solid white; */
+  border-radius: var(--radius);
+  margin: 0 5px;
+}
+.nav-link {
+  color: white;
+  padding: 12px 25px;
+  border-radius: 0;
+  transition: background-color 200ms ease-out;
+}
+.nav-link.nuxt-link-active {
+  font-weight: bold;
+  border-bottom: 5px solid white;
+  /* background-color: #591CAF; */
+}
+
+.profile-dropdown {
+  position: absolute;
+  right: 0;
+}
+.profile {
+  color: grey;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+  z-index: 3;
+}
+.fa-user {
+  padding-top: 20%;
+}
+
+.navigation-items {
+  display: none;
+}
+
+@media (min-width: 768px) {
+  .navigation-items {
+    display: block;
+  }
+}
+</style>
---
+++ _infox/src/components/Navigation/Sidebar.vue
@@ -0,0 +1,109 @@
+<template>
+<div class="sidenav-container">
+  <div
+    v-if="show"
+    class="sidenav-backdrop"
+    @click="$emit('close')"></div>
+  <transition name="slide-side">
+    <div
+      v-if="show"
+      class="sidenav">
+      <ul
+        class="nav-list"
+        @click="$emit('close')">
+        <li class="nav-item">
+          <AppButton btnStyle="btn-accent2" class="m-4" @click="$store.dispatch('event/switchEvent')" >Switch Event</AppButton>
+        </li>
+        <hr>
+        <li class="nav-item">
+          <nuxt-link to="/" class="nav-link">Home</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/network" class="nav-link">Network</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/questions" class="nav-link">Questions</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/polls" class="nav-link">Polls</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/feedback" class="nav-link">Feedback</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </transition>
+</div>
+</template>
+
+<script>
+export default {
+  name: "TheSidenav",
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.sidenav-container {
+  height: 100%;
+  width: 100%;
+}
+
+.sidenav-backdrop {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+.sidenav {
+  height: 100%;
+  width: 300px;
+  background-color: white;
+  z-index: 10000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+}
+
+.slide-side-enter-active,
+.slide-side-leave-active {
+  transition: all 0.3s ease-out;
+}
+.slide-side-enter,
+.slide-side-leave-to {
+  transform: translateX(-100%);
+}
+
+.nav-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.nav-item {
+  margin: 20px 0;
+}
+
+.nav-item a {
+  text-decoration: none;
+  color: black;
+  font-size: 1.2rem;
+}
+
+.nav-item a:hover,
+.nav-item a:active {
+  color: #EB2B63;
+}
+
+</style>
