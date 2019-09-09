4b38d3b908061da5a4de79515c92d9901bba6745
Dhruv <dhruvmisra@live.com>
2019-09-10 01:13:06 +0530

    Added basic glimpses structure

--- frontend/src/components/Glimpses.vue
+++ frontend/src/components/Glimpses.vue
@@ -2,7 +2,29 @@
   <div id="glimpses">
     <Heading anchor=".glimpses" :speed="-3">Glimpses</Heading>
     <div class="glimpses">
-
+      <div class="container">
+        <div class="row m-0 mx-auto justify-content-around text-center">
+          <div class="col-md-12 col-lg-3">
+            <div class="stat">
+              <p class="number m-0">10,000</p>
+              <p>Stat 1</p>
+            </div>
+          </div>
+          <div class="col-md-12 col-lg-3">
+            <div class="stat">
+              <p class="number m-0">5,000</p>
+              <p>Stat 2</p>
+            </div>
+          </div>
+          <div class="col-md-12 col-lg-3">
+            <div class="stat">
+              <p class="number m-0">4,000</p>
+              <p>Stat 3</p>
+            </div>
+          </div>
+        </div>
+        <p class="text my-5 p-5 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, tempora quo pariatur nulla consequuntur ratione quia esse? Repellendus, natus officiis.</p>
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +39,14 @@ export default {};
   margin-top: -40%;
 }
 .glimpses {
-  margin-top: 40%;
+  margin-top: 60%;
+  color: white;
+}
+.number {
+  font-size: 4em;
+  font-weight: bold;
+}
+.text {
+  font-size: 2.5em;
 }
 </style>
