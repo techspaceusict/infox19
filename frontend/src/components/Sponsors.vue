14092983752a47900e624e993ef07553a4e4074a
Dhruv <dhruvmisra@live.com>
2019-09-09 20:15:59 +0530

    Added sponsor images

--- frontend/src/components/Sponsors.vue
+++ frontend/src/components/Sponsors.vue
@@ -5,56 +5,56 @@
       <div class="absoulte layer-1" v-rellax="{speed: 1}">
         <div class="container-fluid">
           <div class="row justify-content-around">
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
+            <div class="sponsor-item barista"></div>
+            <div class="sponsor-item codingBlock"></div>
+            <div class="sponsor-item codingNinja"></div>
           </div>
           <div class="row justify-content-around">
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
+            <div class="sponsor-item Paytm"></div>
+            <div class="sponsor-item redbull"></div>
+            <div class="sponsor-item reliance"></div>
           </div>
           <div class="row justify-content-around">
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
+            <div class="sponsor-item incubateind"></div>
+            <div class="sponsor-item redwolf"></div>
+            <div class="sponsor-item codeStreet"></div>
           </div>
         </div>
       </div>
       <div class="absoulte layer-2" v-rellax="{speed: -1}">
         <div class="container-fluid">
           <div class="row justify-content-around">
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
+            <div class="sponsor-item agro"></div>
+            <div class="sponsor-item designcut"></div>
+            <div class="sponsor-item earnings_lab"></div>
           </div>
           <div class="row justify-content-around">
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
+            <div class="sponsor-item enactus_igdtu"></div>
+            <div class="sponsor-item domainX"></div>
+            <div class="sponsor-item blogx"></div>
           </div>
           <div class="row justify-content-around">
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
+            <div class="sponsor-item gamex"></div>
+            <div class="sponsor-item gaming"></div>
+            <div class="sponsor-item Hidden_Hour"></div>
           </div>
         </div>
       </div>
       <div class="absoulte layer-3" v-rellax="{speed: -3}">
         <div class="container-fluid">
           <div class="row justify-content-around">
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
+            <div class="sponsor-item ED"></div>
+            <div class="sponsor-item HouseOfAmigos"></div>
+            <div class="sponsor-item josh-logo-square"></div>
           </div>
           <div class="row justify-content-around">
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
+            <div class="sponsor-item microHost"></div>
+            <div class="sponsor-item Podio"></div>
+            <div class="sponsor-item reappoint"></div>
           </div>
           <div class="row justify-content-around">
-            <div class="sponsor-item"></div>
-            <div class="sponsor-item"></div>
+            <div class="sponsor-item streetHack"></div>
+            <div class="sponsor-item wall"></div>
             <div class="sponsor-item"></div>
           </div>
         </div>
@@ -86,38 +86,123 @@ export default {
   width: 100%;
 }
 .sponsor-item {
-  background: white;
-  opacity: 0.3;
-  border-radius: 50%;
-  box-shadow: 0px 0px 20px blue;
+  // border-radius: 50%;
   // margin: 50px 100px;
-  margin: 50px 0;
+  margin: 80px 0;
+  // box-shadow: 0px 0px 20px blue;
+  filter: drop-shadow(0px 0px 10px rgba(0, 0, 255, 0.8));
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
 }
+
+.barista {
+  background-image: url('../assets/sponsors/barista.jpg');
+}
+.agro {
+  background-image: url('../assets/sponsors/agro.jpg');
+}
+.blogx {
+  background-image: url('../assets/sponsors/blogx.jpg');
+}
+.codeStreet {
+  background-image: url('../assets/sponsors/codeStreet.png');
+}
+.codingBlock {
+  background-image: url('../assets/sponsors/codingBlock.png');
+}
+.codingNinja {
+  background-image: url('../assets/sponsors/codingNinja.svg');
+}
+.designcut {
+  background-image: url('../assets/sponsors/designcut.png');
+}
+.domainX {
+  background-image: url('../assets/sponsors/domainX.png');
+}
+.earning_labs {
+  background-image: url('../assets/sponsors/earning_labs.png');
+}
+.ED {
+  background-image: url('../assets/sponsors/ED.png');
+}
+.enactus_igdtu {
+  background-image: url('../assets/sponsors/enactus_igdtu.png');
+}
+.gamex {
+  background-image: url('../assets/sponsors/gamex.jpg');
+}
+.gaming {
+  background-image: url('../assets/sponsors/gaming.jpg');
+}
+.Hidden_Hour {
+  background-image: url('../assets/sponsors/Hidden_Hour.png');
+}
+.HouseOfAmigos {
+  background-image: url('../assets/sponsors/HouseOfAmigos.png');
+}
+.incubateind {
+  background-image: url('../assets/sponsors/incubateind.png');
+}
+.josh-logo-square {
+  background-image: url('../assets/sponsors/josh-logo-square.png');
+}
+.microHost {
+  background-image: url('../assets/sponsors/microHost.png');
+}
+.Paytm {
+  background-image: url('../assets/sponsors/Paytm.png');
+}
+.Podio {
+  background-image: url('../assets/sponsors/Podio.png');
+}
+.reappoint {
+  background-image: url('../assets/sponsors/reappoint.jpg');
+}
+.redbull {
+  background-image: url('../assets/sponsors/redbull.png');
+}
+.redwolf {
+  background-image: url('../assets/sponsors/redwolf.png');
+}
+.reliance {
+  background-image: url('../assets/sponsors/reliance.png');
+}
+.streetHack {
+  background-image: url('../assets/sponsors/streetHack.jpg');
+}
+.wall {
+  background-image: url('../assets/sponsors/wall.png');
+}
+
+
 .layer-1 {
   margin-top: 0;
+  z-index: 3;
 }
 .layer-2 {
   margin-top: -10em;
+  z-index: 2;
 }
 .layer-3 {
   margin-top: -30em;
+  z-index: 1;
 }
 
 .layer-1 .sponsor-item {
-  width: 10em;
-  height: 10em;
-  background: rgb(41, 41, 233);
-  opacity: 0.9;
+  width: 12em;
+  height: 12em;
+  opacity: 1;
 }
 .layer-2 .sponsor-item {
   width: 8em;
   height: 8em;
-  background: rgb(41, 41, 233);
-  opacity: 0.5;
+  opacity: 0.9;
 }
 .layer-3 .sponsor-item {
   width: 5em;
   height: 5em;
+  opacity: 0.8;
 }
 
 .layer-1 .row:nth-child(even) {
@@ -135,4 +220,9 @@ export default {
 .layer-3 .row:nth-child(even) {
   transform: translateX(120px);
 }
+
+// .sponsor-item:nth-child(1) {
+//   background-image: url('../assets/sponsors/barista.jpg');
+//   background-size: cover;
+// }
 </style>
