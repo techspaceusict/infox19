b09d6933562c2bc58f592615e594e348c49ee4d6
aakash sangwan <aakashsangwan024@gmail.com>
2019-09-18 19:34:35 +0530

    added background to events

--- frontend/src/views/Events.vue
+++ frontend/src/views/Events.vue
@@ -1,4 +1,5 @@
-<template>
+<template><div class="context" >
+      <ul class="circles">
     <div id="events">
     <h1 class="text-center text-white">Events</h1>
     
@@ -29,8 +30,33 @@
         </div>
     </div> -->
     
-    
     </div>
+    
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    
+  </ul>
+  </div>
+    
+    
 </template>
 
 <script>
@@ -55,6 +81,210 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+template{
+    position: fixed;
+    background-attachment: fixed;
+}
+
+.context {
+    width: 100%;
+    
+    top:50vh;
+    
+    background-attachment: fixed;
+    
+}
+
+.area{
+    background: #4e54c8;  
+    background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);  
+    width: 100vw;
+    height:1000vh;
+    position: fixed;
+    background-attachment: fixed;
+   
+}
+
+
+
+
+
+.circles li{
+    position: absolute;
+    display: block;
+    list-style: none;
+    width: 20px;
+    height: 20px;
+    background: rgba(255, 255, 255, 0.2);
+    animation: animate 25s linear infinite;
+    bottom: -150px;
+    position: fixed;
+    
+    
+}
+
+.circles li:nth-child(1){
+    left: 25%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 4s;
+}
+
+
+.circles li:nth-child(2){
+    left: 10%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 2s;
+    animation-duration: 12s;
+}
+
+.circles li:nth-child(3){
+    left: 70%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 4s;
+}
+
+.circles li:nth-child(4){
+    left: 40%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 0s;
+    animation-duration: 18s;
+}
+
+.circles li:nth-child(5){
+    left: 65%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 0s;
+}
+
+.circles li:nth-child(6){
+    left: 75%;
+    width: 90px;
+    height: 90px;
+    animation-delay: 3s;
+}
+
+.circles li:nth-child(7){
+    left: 35%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 7s;
+}
+
+.circles li:nth-child(8){
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+}
+
+.circles li:nth-child(9){
+    left: 20%;
+    width: 15px;
+    height: 15px;
+    animation-delay: 2s;
+    animation-duration: 35s;
+}
+
+.circles li:nth-child(10){
+    left: 85%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 0s;
+    animation-duration: 11s;
+}
+.circles li:nth-child(11){
+    left: 25%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 4s;
+}
+.circles li:nth-child(12){
+    left: 25%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 4s;
+}
+.circles li:nth-child(13){
+    left: 25%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 4s;
+}
+.circles li:nth-child(14){
+    left: 25%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 4s;
+}
+.circles li:nth-child(15){
+    left: 25%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 4s;
+}
+
+.circles li:nth-child(16){
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+}
+.circles li:nth-child(17){
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+}
+.circles li:nth-child(18){
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+}
+.circles li:nth-child(19){
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+}
+.circles li:nth-child(20){
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+}
+
+
+@keyframes animate {
+
+    0%{
+        transform: translateY(0) rotate(0deg);
+        opacity: 1;
+        border-radius: 0;
+    }
+
+    100%{
+        transform: translateY(-1000px) rotate(720deg);
+        opacity: 0.6;
+        border-radius: 50%;
+    }
+
+}
+
+
+
 // #events *{z-index: 2;}
 #events{
     width:90%;
