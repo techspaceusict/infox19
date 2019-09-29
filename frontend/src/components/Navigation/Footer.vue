b62d85cbc12705e6f9652d1980b9474946402fa6
aakash sangwan <aakashsangwan024@gmail.com>
2019-09-29 18:36:20 +0530

    updates

--- frontend/src/components/Navigation/Footer.vue
+++ frontend/src/components/Navigation/Footer.vue
@@ -67,7 +67,8 @@
         <section class="tex-center">
           <p class="font-weight-bold font-sm mb-2">FOLLOW US</p>
           <div class="social-row row m-0 py-2">
-            <a href="https://www.facebook.com/infoxpression/?ref=br_rs" style="color:white;"><i class="fab fa-facebook-square fa-lg"></i></a>
+            <a href="https://www.facebook.com/infoxpression/?ref=br_rs" 
+            style="color:white;"><i class="fab fa-facebook-square fa-lg"></i></a>
             <i></i>
             <a href="https://www.instagram.com/infoxggsipu/" style="color:white;"><i class="fab fa-instagram fa-lg"></i></a>
             <i></i>
